88617b703f15b10a686e35fd4cfe03e62068a6ab
hoddiemaster <79085243+hoddiemaster@users.noreply.github.com>
2021-06-10 13:06:44 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,59 @@
+canvas= document.getElementById("my-canvas");
+ctx= canvas.getContext("2d");
+color="blue";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+
+color="black";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(285,200,40,0,2*Math.PI);
+ctx.stroke();
+
+color="yellow";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(240,250,40,0,2*Math.PI);
+ctx.stroke();
+
+color="red";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(370,200,40,0,2*Math.PI);
+ctx.stroke();
+
+color="green";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(340,240,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e){
+color=document.getElementById("color").value;
+    mouse_x=e.clientX-canvas.offsetLeft;
+    mouse_y=e.clientY-canvas.offsetTop;
+    circle(mouse_x,mouse_y);
+}
+function circle(mouse_x,mouse_y){
+    ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
+ctx.stroke();
+}
+function clearcanvas(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
